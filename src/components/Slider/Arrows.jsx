8505cd38af5176e2d00fae74df1468100fdf7b31
Goddo-ro <r0manovic1@yandex.ru>
2023-09-12 20:44:34 +0300

    change offset when we click on arrows

--- src/components/Slider/Arrows.jsx
+++ src/components/Slider/Arrows.jsx
@@ -1,10 +1,22 @@
-import {SlArrowLeft, SlArrowRight} from "react-icons/sl";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+import $store, { setOffset } from "./store.js";
+import { useStore } from "effector-react";
 
 export default function Arrows() {
+  const store = useStore($store);
+
   return (
     <>
-      <SlArrowLeft className="slider__arrow slider__arrow_left" />
-      <SlArrowRight className="slider__arrow slider__arrow_right" />
+      <SlArrowLeft className="slider__arrow slider__arrow_left"
+                   onClick={() => {
+                     setOffset(store.offset + store.width);
+                   }}
+      />
+      <SlArrowRight className="slider__arrow slider__arrow_right"
+                    onClick={() => {
+                      setOffset(store.offset - store.width);
+                    }}
+      />
     </>
   )
 }
